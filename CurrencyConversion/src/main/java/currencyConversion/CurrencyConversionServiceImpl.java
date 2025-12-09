2d3aa68bf3e0a7cd83c5b759b39a3b851b212360
OgnjenJovanovicc <ognjenjov02@gmail.com>
2025-12-09 13:11:17 +0100

    vezbe 5

--- CurrencyConversion/src/main/java/currencyConversion/CurrencyConversionServiceImpl.java
+++ CurrencyConversion/src/main/java/currencyConversion/CurrencyConversionServiceImpl.java
@@ -3,6 +3,7 @@ package currencyConversion;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,10 @@ import api.dtos.CurrencyConversionDto;
 import api.dtos.CurrencyExchangeDto;
 import api.proxies.CurrencyExchangeProxy;
 import api.services.CurrencyConversionService;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
 import util.exceptions.InvalidQuantityException;
 
 @RestController
@@ -21,6 +26,32 @@ public class CurrencyConversionServiceImpl implements CurrencyConversionService 
 	@Autowired
 	private CurrencyExchangeProxy proxy;
 	
+	Retry retry;
+	CurrencyExchangeDto response;
+	
+	public CurrencyConversionServiceImpl(RetryRegistry registry) {
+		retry=registry.retry("default");
+	}
+	
+
+	@Override
+	@CircuitBreaker(name="cb",fallbackMethod="fallback")
+	public ResponseEntity<?> getConversionFeign(String from, String to, BigDecimal quantity) {
+		if(quantity.compareTo(BigDecimal.valueOf(300.0))==1) {
+			throw new InvalidQuantityException(String.format("Qunatity of %s is to large", quantity));
+		}
+		
+		retry.executeSupplier(() -> response = proxy.getExchangeFeign(from, to).getBody());
+		
+		
+		CurrencyConversionDto finalResponse=new CurrencyConversionDto(response,quantity);
+		finalResponse.setFeign(true);
+		return ResponseEntity.ok(finalResponse);
+	}
+	
+	public ResponseEntity<?> fallback(CallNotPermittedException ex){
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Currency conversion service is currently unavailbale,Circuit is in OPEN state!");
+	}
 	@Override
 	public ResponseEntity<?> getConversion(String from, String to, BigDecimal quantity) {
 		
@@ -30,18 +61,6 @@ public class CurrencyConversionServiceImpl implements CurrencyConversionService 
 		String endPoint ="http://localhost:8000/currency-exchange?from="+ from + "&to=" +to;
 	ResponseEntity<CurrencyExchangeDto>response = template.getForEntity(endPoint, CurrencyExchangeDto.class);
 	return ResponseEntity.ok(new CurrencyConversionDto(response.getBody(),quantity));
-	}
-
-	@Override
-	public ResponseEntity<?> getConversionFeign(String from, String to, BigDecimal quantity) {
-		if(quantity.compareTo(BigDecimal.valueOf(300.0))==1) {
-			throw new InvalidQuantityException(String.format("Qunatity of %s is to large", quantity));
-		}
-		ResponseEntity<CurrencyExchangeDto>response=proxy.getExchangeFeign(from, to);
-		
-		CurrencyConversionDto finalResponse=new CurrencyConversionDto(response.getBody(),quantity);
-		finalResponse.setFeign(true);
-		return ResponseEntity.ok(finalResponse);
 	}
 
 }
